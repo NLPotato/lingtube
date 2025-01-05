88bf75dca28352872a774efc4454bc272c6cf4a6
wjlee <wjlee.ling@gmail.com>
2025-01-05 17:18:14 +0900

    feat: added fonts for Japanese and Korean

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -16,6 +16,22 @@ export default function RootLayout() {
     "Poppins-Regular": require("@/assets/fonts/Poppins-Regular.ttf"),
     "Poppins-SemiBold": require("@/assets/fonts/Poppins-SemiBold.ttf"),
     "Poppins-Thin": require("@/assets/fonts/Poppins-Thin.ttf"),
+    "NotoSansJP-Thin": require("@/assets/fonts/NotoSansJP-Thin.ttf"),
+    "NotoSansJP-Light": require("@/assets/fonts/NotoSansJP-Light.ttf"),
+    "NotoSansJP-Regular": require("@/assets/fonts/NotoSansJP-Regular.ttf"),
+    "NotoSansJP-Medium": require("@/assets/fonts/NotoSansJP-Medium.ttf"),
+    "NotoSansJP-SemiBold": require("@/assets/fonts/NotoSansJP-SemiBold.ttf"),
+    "NotoSansJP-Bold": require("@/assets/fonts/NotoSansJP-Bold.ttf"),
+    "NotoSansJP-ExtraBold": require("@/assets/fonts/NotoSansJP-ExtraBold.ttf"),
+    "NotoSansJP-Black": require("@/assets/fonts/NotoSansJP-Black.ttf"),
+    "NotoSansKR-Thin": require("@/assets/fonts/NotoSansKR-Thin.ttf"),
+    "NotoSansKR-Light": require("@/assets/fonts/NotoSansKR-Light.ttf"),
+    "NotoSansKR-Regular": require("@/assets/fonts/NotoSansKR-Regular.ttf"),
+    "NotoSansKR-Medium": require("@/assets/fonts/NotoSansKR-Medium.ttf"),
+    "NotoSansKR-SemiBold": require("@/assets/fonts/NotoSansKR-SemiBold.ttf"),
+    "NotoSansKR-Bold": require("@/assets/fonts/NotoSansKR-Bold.ttf"),
+    "NotoSansKR-ExtraBold": require("@/assets/fonts/NotoSansKR-ExtraBold.ttf"),
+    "NotoSansKR-Black": require("@/assets/fonts/NotoSansKR-Black.ttf"),
   });
 
   useEffect(() => {
