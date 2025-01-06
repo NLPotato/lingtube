1ae2bfeeea7b46e62b483c2f281426142b86ee9c
wjlee <wjlee.ling@gmail.com>
2025-01-06 20:29:26 +0900

    feat: added SafeViewArea for (tabs)

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,31 +1,34 @@
 import { Tabs } from "expo-router";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: true }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons
-              name={focused ? "home-sharp" : "home-outline"}
-              size={24}
-              color={color}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="new"
-        options={{
-          title: "New",
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="add-circle" size={24} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+    <SafeAreaView className="flex-1">
+      <Tabs screenOptions={{ headerShown: true }}>
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Home",
+            tabBarIcon: ({ color, focused }) => (
+              <Ionicons
+                name={focused ? "home-sharp" : "home-outline"}
+                size={24}
+                color={color}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="new"
+          options={{
+            title: "New",
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="add-circle" size={24} color={color} />
+            ),
+          }}
+        />
+      </Tabs>
+    </SafeAreaView>
   );
 }
