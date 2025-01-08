9999d330cd20879d3efdc9e7b5124542a1dccd7b
wjlee <wjlee.ling@gmail.com>
2025-01-09 00:27:12 +0900

    update: changed Button component to allow props in a more general way

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,16 +1,17 @@
-import { StyleSheet, View, Pressable, Text } from "react-native";
+import { View, Pressable, Text } from "react-native";
 
-type Props = {
-  label: string;
+type ButtonProps = {
+  props: string | React.ReactNode;
   onPress?: () => void;
   disabled?: boolean;
 };
 
-const Button = ({ label, onPress, disabled }: Props) => {
+const Button = ({ props, onPress, disabled }: ButtonProps) => {
   return (
-    <View className={`w-[320px] h-[70px] flex bg-secondary ${disabled ? 'bg-opacity-50' : ''} justify-center items-center rounded-md`}>
+    <View className={`w-full h-full flex bg-secondary ${disabled ? 'bg-opacity-50' : ''} justify-center items-center rounded-md`}>
       <Pressable onPress={onPress} disabled={disabled}>
-        <Text className="text-white text-2xl font-bold">{label}</Text>
+        {props}
+        {/* <Text className={`text-white ${textStyle ? textStyle : ''} font-bold`}>{props}</Text> */}
       </Pressable>
     </View>
   );
