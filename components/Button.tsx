2dcfe8dc4a83689c8070d0b2e0df8c816b68f34c
wjlee <wjlee.ling@gmail.com>
2025-01-09 14:18:06 +0900

    feat: update Button comp with  state

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,4 +1,7 @@
+"use client";
+import { useState } from "react";
 import { View, Pressable } from "react-native";
+import clsx from "clsx";
 
 type ButtonProps = {
   props: string | React.ReactNode;
@@ -7,16 +10,20 @@ type ButtonProps = {
 };
 
 const Button = ({ props, onPress, disabled }: ButtonProps) => {
+  const [isPressed, setIsPressed] = useState(false);
+
+  const buttonClasses = clsx("w-full h-full flex-1 bg-secondary rounded-md", {
+    "opacity-50": isPressed || disabled,
+  });
+
   return (
-    <View
-      className={`w-full h-full flex bg-secondary ${
-        disabled ? "bg-opacity-30" : ""
-      } justify-center items-center rounded-md`}
-    >
+    <View className={buttonClasses}>
       <Pressable
         onPress={onPress}
         disabled={disabled}
         className="w-full h-full items-center justify-center"
+        onPressIn={() => setIsPressed(true)}
+        onPressOut={() => setIsPressed(false)}
       >
         {props}
       </Pressable>
