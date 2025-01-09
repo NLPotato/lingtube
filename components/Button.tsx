6cc842290a600c304aa52e01a740281917eae242
wjlee <wjlee.ling@gmail.com>
2025-01-09 13:53:53 +0900

    refactor: changed Button components to allow for custom types instead of text-label

--- components/Button.tsx
+++ components/Button.tsx
@@ -8,10 +8,17 @@ type ButtonProps = {
 
 const Button = ({ props, onPress, disabled }: ButtonProps) => {
   return (
-    <View className={`w-full h-full flex bg-secondary ${disabled ? 'bg-opacity-50' : ''} justify-center items-center rounded-md`}>
-      <Pressable onPress={onPress} disabled={disabled}>
+    <View
+      className={`w-full h-full flex bg-secondary ${
+        disabled ? "bg-opacity-30" : ""
+      } justify-center items-center rounded-md`}
+    >
+      <Pressable
+        onPress={onPress}
+        disabled={disabled}
+        className="w-full h-full items-center justify-center"
+      >
         {props}
-        {/* <Text className={`text-white ${textStyle ? textStyle : ''} font-bold`}>{props}</Text> */}
       </Pressable>
     </View>
   );
